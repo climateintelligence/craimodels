e2e9d2736b80bd3e013822aef74a49910980b804
Étienne Plésiat <plesiat@dkrz.de>
2023-09-05 18:18:38 +0200

    Adapt yaml file for phoenix metadata

--- craimodels/main.py
+++ craimodels/main.py
@@ -2,6 +2,9 @@ import pkgutil
 import pathlib
 import yaml
 
+def raw_text():
+    return pkgutil.get_data(__name__, "models/info-models.yml")
+
 def info_models():
     path = pkgutil.get_data(__name__, "models/info-models.yml")
     return yaml.safe_load(path)
