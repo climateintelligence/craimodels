272d4b8cc87bd78b9b5dd992091c403d7e597f5e
Étienne Plésiat <plesiat@dkrz.de>
2024-01-23 16:45:40 +0100

    Add function to convert YAML file to metadata structure for duck

--- craimodels/main.py
+++ craimodels/main.py
@@ -11,3 +11,14 @@ def info_models():
 
 def model_dir():
     return pathlib.Path(__file__).parent / "models/"
+
+def metadata():
+    info_dict = {}
+    info = info_models()
+    for key in info.keys():
+        info_dict[key] = [
+            {"name": "dataset_name", "value": key, "title": info[key]["description"]},
+            {"name": "file", "value": info[key]["file"], "title": "Enter the URL of the netcdf file or ZIP archive."},
+            {"name": "variable_name", "value": info[key]["variable_name"], "title": info[key]["variable_name"]}
+        ]
+    return info_dict
